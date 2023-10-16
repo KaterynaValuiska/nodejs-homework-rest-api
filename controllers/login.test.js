d4372877840673b650c07fbcafe1bd23340173b6
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-10-16 11:25:50 +0200

    Update login.test.js

--- controllers/login.test.js
+++ controllers/login.test.js
@@ -3,7 +3,7 @@ import login from "./functions/login.js";
 // function sum(a, b) {
 //   return a + b;
 // }
-console.log(login);
+
 describe("test login function", () => {
   //   test("adds 1 + 2 to equal 3", () => {
   //     expect(sum(1, 2)).toBe(3);
